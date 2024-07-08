f71635317f1831c7949dbe82ee5c8290ab305c90
Lucas Melo <lucas.melo044@gmail.com>
2024-07-08 17:08:20 -0300

    test: ensure ProductCreateController calls AddProduct with correct parameters

--- src/presentation/controller/product-create/product-create-controller.spec.ts
+++ src/presentation/controller/product-create/product-create-controller.spec.ts
@@ -1,7 +1,9 @@
 import { HttpRequest } from '../../protocols/http-request-protocol'
+import { AddProduct, AddProductModel } from '../../../domain/usecase/add-product'
 import { Validation } from '../../protocols/validation-protocol'
 
 import { ProductCreateController } from './product-create-controller'
+import { ProductModel } from '../../../domain/models/product-model'
 
 function makeValidationStub (): Validation {
   class ValidationStub implements Validation {
@@ -13,10 +15,25 @@ function makeValidationStub (): Validation {
   return new ValidationStub()
 }
 
+function makeAddProductStub (): AddProduct {
+  class AddProductStub implements AddProduct {
+    async add (addProductModel: AddProductModel): Promise<ProductModel> {
+      return await new Promise(resolve => resolve({
+        id: 'any-id',
+        name: 'any-name',
+        barCode: 'any-bar-code',
+        imageUrl: 'any-image-',
+        price: 23.1
+      }))
+    }
+  }
+
+  return new AddProductStub()
+}
+
 function makeFakeHttpRequest (): HttpRequest {
   return {
     body: {
-      id: 'any_id',
       name: 'any_name',
       barCode: 'any_bar_code',
       imageUrl: 'any_image_url',
@@ -28,14 +45,17 @@ function makeFakeHttpRequest (): HttpRequest {
 interface SutTypes {
   sut: ProductCreateController
   validationStub: Validation
+  addProductStub: AddProduct
 }
 
 function makeSut (): SutTypes {
   const validationStub = makeValidationStub()
-  const sut = new ProductCreateController(validationStub)
+  const addProductStub = makeAddProductStub()
+  const sut = new ProductCreateController(validationStub, addProductStub)
   return {
     sut,
-    validationStub
+    validationStub,
+    addProductStub
   }
 }
 
@@ -56,5 +76,13 @@ describe('ProductCreate Controller', () => {
       status: 400,
       body: new Error('any error')
     })
+  })
+
+  it('should call AddProduct with correct parameters', async () => {
+    const { sut, addProductStub } = makeSut()
+    const addSpy = jest.spyOn(addProductStub, 'add')
+    const request = makeFakeHttpRequest()
+    await sut.handle(request)
+    expect(addSpy).toHaveBeenCalledWith(request.body)
   })
 })
