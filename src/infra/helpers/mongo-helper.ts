88f5425c905fd261ce0f3f301b92cc0398c82d5e
Lucas Melo <lucas.melo044@gmail.com>
2024-07-19 18:28:24 -0300

    feat: add insertAndFind and map methods

--- src/infra/helpers/mongo-helper.ts
+++ src/infra/helpers/mongo-helper.ts
@@ -1,10 +1,26 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
+import { ProductModel } from '../../domain/models/product-model'
 
 export const MongoHelper = {
   client: null as MongoClient,
 
   async connect (url: string): Promise<void> {
     this.client = await MongoClient.connect(url)
+  },
+
+  getCollection (collectionName: string): Collection {
+    return this.client.db().collection(collectionName)
+  },
+
+  async insertAndFind (data: any, collectionName: string): Promise<any> {
+    const collection = this.getCollection(collectionName)
+    const { insertedId } = await collection.insertOne(data)
+    return collection.findOne({ _id: insertedId })
+  },
+
+  map (product: any): ProductModel {
+    const { _id, ...productWithoutId } = product
+    return { id: _id.toString(), ...productWithoutId }
   },
 
   async disconnect (): Promise<void> {
