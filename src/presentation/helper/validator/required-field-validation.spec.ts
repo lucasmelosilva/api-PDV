868c8a27a58c1bc1608993e87fb7275772565d4c
Lucas Melo <lucas.melo044@gmail.com>
2024-08-12 19:33:52 -0300

    test: add RequiredFieldValidation

---
+++ src/presentation/helper/validator/required-field-validation.spec.ts
@@ -0,0 +1,20 @@
+import { MissingParamError } from '../../errors/missing-param-error'
+import { RequiredFieldValidation } from './required-field-validation'
+
+const makeSut = (): RequiredFieldValidation => (
+  new RequiredFieldValidation('field')
+)
+
+describe('RequiredField Validation', () => {
+  it('should return a MissingParamsError if validate fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({})
+    expect(error).toBeInstanceOf(MissingParamError)
+  })
+
+  it('should not return if validate succeeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
+})
