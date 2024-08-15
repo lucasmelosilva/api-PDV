1fec3914630c0bd221f5dc3c8a7db835804d679b
Lucas Melo <lucas.melo044@gmail.com>
2024-08-15 20:13:33 -0300

    test: ensure ProductMongoRepository returns null if product does not exist

--- src/infra/db/mongodb/product/product-mongo-repository.spec.ts
+++ src/infra/db/mongodb/product/product-mongo-repository.spec.ts
@@ -1,5 +1,8 @@
 import { ProductMongoRepository } from './product-mongo-repository'
 import { MongoHelper } from '../../../helpers/mongo-helper'
+import { Collection } from 'mongodb'
+
+let productCollection: Collection
 
 describe('ProductMongoRepository', () => {
   beforeAll(async () => {
@@ -11,19 +14,34 @@ describe('ProductMongoRepository', () => {
     await MongoHelper.disconnect()
   })
 
-  it('should return a product on success', async () => {
-    const sut = new ProductMongoRepository()
-    const result = await sut.addProduct({
-      name: 'any_name',
-      barCode: 'any_bar_code',
-      imageUrl: 'any_image_url',
-      price: 212.32
+  beforeEach(async () => {
+    productCollection = MongoHelper.getCollection('products')
+    await productCollection.deleteMany({})
+  })
+
+  describe('add()', () => {
+    it('should return a product on success', async () => {
+      const sut = new ProductMongoRepository()
+      const result = await sut.addProduct({
+        name: 'any_name',
+        barCode: 'any_bar_code',
+        imageUrl: 'any_image_url',
+        price: 212.32
+      })
+      expect(result).toBeTruthy()
+      expect(result.id).toBeTruthy()
+      expect(result.name).toBe('any_name')
+      expect(result.barCode).toBe('any_bar_code')
+      expect(result.imageUrl).toBe('any_image_url')
+      expect(result.price).toBe(212.32)
     })
-    expect(result).toBeTruthy()
-    expect(result.id).toBeTruthy()
-    expect(result.name).toBe('any_name')
-    expect(result.barCode).toBe('any_bar_code')
-    expect(result.imageUrl).toBe('any_image_url')
-    expect(result.price).toBe(212.32)
+  })
+
+  describe('loadByBarCode()', () => {
+    it('should return null if product does not exist', async () => {
+      const sut = new ProductMongoRepository()
+      const result = await sut.loadByBarCode('7711889900')
+      expect(result).toBeFalsy()
+    })
   })
 })
