3b4b114497df8ae1fd3b55512aa62fafd7e378be
Lucas Melo <lucas.melo044@gmail.com>
2024-08-15 20:43:20 -0300

    refactor: organize factories folders

--- src/main/routes/product-router.ts
+++ src/main/routes/product-router.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { uploadMiddleware } from '../adapters/multer/multer-adapter'
 import { adapterRoute } from '../adapters/express/express-route-adapter'
-import { makeProductCreate } from '../factories/product/make-product-create'
+import { makeProductCreate } from '../factories/product/product-create/make-product-create'
 
 export default (router: Router): void => {
   router.post('/products', uploadMiddleware.single('image'), adapterRoute(makeProductCreate()))
