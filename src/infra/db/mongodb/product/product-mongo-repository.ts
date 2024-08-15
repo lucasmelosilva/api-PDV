fc656997917751777e2100d0742948ba584e49bd
Lucas Melo <lucas.melo044@gmail.com>
2024-08-15 20:13:07 -0300

    feat: ensure ProductMongoRepository returns null if product does not exist

--- src/infra/db/mongodb/product/product-mongo-repository.ts
+++ src/infra/db/mongodb/product/product-mongo-repository.ts
@@ -1,9 +1,14 @@
 import { AddProductRepository } from '../../../../data/protocol/add-product-repository'
+import { LoadProductByBarCodeRepository } from '../../../../data/protocol/load-product-by-bar-code-repository'
 import { ProductModel } from '../../../../domain/models/product-model'
 import { AddProductModel } from '../../../../domain/usecase/add-product'
 import { MongoHelper } from '../../../helpers/mongo-helper'
 
-export class ProductMongoRepository implements AddProductRepository {
+export class ProductMongoRepository implements AddProductRepository, LoadProductByBarCodeRepository {
+  async loadByBarCode (barCode: string): Promise<ProductModel> {
+    return new Promise(resolve => resolve(null))
+  }
+
   async addProduct (product: AddProductModel): Promise<ProductModel> {
     const result = await MongoHelper.insertAndFind(product, 'products')
     return MongoHelper.map(result)
