2198dca2efcedf0cec38111aafa6ad1a92b3819c
Lucas Melo <lucas.melo044@gmail.com>
2024-08-20 14:45:33 -0300

    feat: ensure LoadProductController return 500 if LoadProduct throws

--- src/presentation/controller/load-product/load-product-controller.ts
+++ src/presentation/controller/load-product/load-product-controller.ts
@@ -1,6 +1,7 @@
 import { LoadProduct } from '../../../domain/usecase/load-product'
 import { notFound } from '../../helper/http/not-found'
 import { ok } from '../../helper/http/ok'
+import { serverError } from '../../helper/http/server-error'
 import { Controller } from '../../protocols/controller-protocol'
 import { HttpRequest } from '../../protocols/http-request-protocol'
 import { HttpResponse } from '../../protocols/http-response-protocol'
@@ -9,11 +10,15 @@ export class LoadProductController implements Controller {
   constructor (private readonly loadProduct: LoadProduct) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { barCode } = httpRequest.body
-    const product = await this.loadProduct.load(barCode as string)
-    if (!product) {
-      return notFound(barCode as string)
+    try {
+      const { barCode } = httpRequest.body
+      const product = await this.loadProduct.load(barCode as string)
+      if (!product) {
+        return notFound(barCode as string)
+      }
+      return ok(product)
+    } catch (error) {
+      return serverError(error as Error)
     }
-    return ok(product)
   }
 }
