6e6b2da6ce306b5b1b15e8631b11554fda52ab75
Lucas Melo <lucas.melo044@gmail.com>
2024-08-20 14:49:54 -0300

    test: ensure LoadProductController return 500 if LoadProduct throws

--- src/presentation/controller/load-product/load-product-controller.spec.ts
+++ src/presentation/controller/load-product/load-product-controller.spec.ts
@@ -4,6 +4,8 @@ import { ProductModel } from '../../../domain/models/product-model'
 import { LoadProductController } from './load-product-controller'
 import { notFound } from '../../helper/http/not-found'
 import { ok } from '../../helper/http/ok'
+import { ServerError } from '../../errors/server-error'
+import { serverError } from '../../helper/http/server-error'
 
 function makeLoadProductStub (): LoadProduct {
   class LoadProductStub implements LoadProduct {
@@ -69,5 +71,14 @@ describe('LoadProductController', () => {
       imageUrl: 'any_image_url',
       price: 1.99
     }))
+  })
+
+  it('should return 500 if any AddProduct throws', async () => {
+    const { sut, loadProductStub } = makeSut()
+    jest.spyOn(loadProductStub, 'load').mockImplementationOnce(() => {
+      throw new ServerError('any-stack')
+    })
+    const response = await sut.handle(mockFakeRequest())
+    expect(response).toEqual(serverError(new ServerError('any-stack')))
   })
 })
