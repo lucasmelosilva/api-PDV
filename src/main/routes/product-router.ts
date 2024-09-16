6ec4a92ec316a3a9b3581135866f90b4f59baf72
Lucas Melo <lucas.melo044@gmail.com>
2024-09-15 23:07:49 -0300

    feat: add delete product route

--- src/main/routes/product-router.ts
+++ src/main/routes/product-router.ts
@@ -3,8 +3,10 @@ import { uploadMiddleware } from '../adapters/multer/multer-adapter'
 import { adapterRoute } from '../adapters/express/express-route-adapter'
 import { makeAddProduct } from '../factories/product/add-product/make-add-product'
 import { makeLoadProduct } from '../factories/product/load-product/make-load-product'
+import { makeDeleteProduct } from '../factories/product/delete-product/make-delete-product'
 
 export default (router: Router): void => {
   router.post('/products', uploadMiddleware.single('image'), adapterRoute(makeAddProduct()))
   router.get('/products/:barcode', adapterRoute(makeLoadProduct()))
+  router.delete('/products/:barcode', adapterRoute(makeDeleteProduct()))
 }
