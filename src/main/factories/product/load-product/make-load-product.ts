2368d7493896885e3e14c2b710d3d1fbfb28827c
Lucas Melo <lucas.melo044@gmail.com>
2024-09-15 22:57:42 -0300

    refactor: remane makeDbLoadProduct

--- src/main/factories/product/load-product/make-load-product.ts
+++ src/main/factories/product/load-product/make-load-product.ts
@@ -1,4 +1,4 @@
-import { makeDbLoadProduct } from '../../db/db-load-product/db-load-product'
+import { makeDbLoadProduct } from '../../db/db-load-product/make-db-load-product'
 import { LoadProductController } from '../../../../presentation/controller/load-product/load-product-controller'
 import { Controller } from '../../../../presentation/protocols/controller-protocol'
 
