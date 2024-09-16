c7111d7a91ad9c8095ea3c968ab5f5255289b8fa
Lucas Melo <lucas.melo044@gmail.com>
2024-09-15 21:25:53 -0300

    feat: ensure ProductMongoRepository delete product by barCode

--- src/infra/db/mongodb/product/product-mongo-repository.ts
+++ src/infra/db/mongodb/product/product-mongo-repository.ts
@@ -1,10 +1,11 @@
 import { AddProductRepository } from '../../../../data/protocol/add-product-repository'
+import { DeleteProductRepository } from '../../../../data/protocol/delete-product-repository'
 import { LoadProductByBarCodeRepository } from '../../../../data/protocol/load-product-by-bar-code-repository'
 import { ProductModel } from '../../../../domain/models/product-model'
 import { AddProductModel } from '../../../../domain/usecase/add-product'
 import { MongoHelper } from '../../../helpers/mongo-helper'
 
-export class ProductMongoRepository implements AddProductRepository, LoadProductByBarCodeRepository {
+export class ProductMongoRepository implements AddProductRepository, LoadProductByBarCodeRepository, DeleteProductRepository {
   async loadByBarCode (barCode: string): Promise<ProductModel> {
     const productCollection = MongoHelper.getCollection('products')
     const result = await productCollection.findOne({ barCode })
@@ -14,5 +15,10 @@ export class ProductMongoRepository implements AddProductRepository, LoadProduct
   async addProduct (product: AddProductModel): Promise<ProductModel> {
     const result = await MongoHelper.insertAndFind(product, 'products')
     return MongoHelper.map(result)
+  }
+
+  async delete (barCode: string): Promise<void> {
+    const productCollection = MongoHelper.getCollection('products')
+    await productCollection.deleteOne({ barCode })
   }
 }
