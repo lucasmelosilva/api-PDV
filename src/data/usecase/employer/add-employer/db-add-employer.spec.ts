836b1b058133e2c6bb87813f0f0ed0cfaa65cc89
Lucas Melo <lucas.melo044@gmail.com>
2025-02-11 20:40:26 -0300

    test: ensure DbAddEmployer calls AddEmployerRepository with correct values

---
+++ src/data/usecase/employer/add-employer/db-add-employer.spec.ts
@@ -0,0 +1,52 @@
+import { EmployerModel } from '../../../../domain/models/employer-model'
+import { AddEmployerModel } from '../../../../domain/usecase/employer/add-employer'
+import { AddEmployerRepository } from '../../../protocol/employer/add-employer-repository'
+import { DbAddEmployer } from './db-add-employer'
+
+function makeAddEmployerRepositoryStub (): AddEmployerRepository {
+  class AddEmployerRepositoryStub implements AddEmployerRepository {
+    async addEmployer (addEmployerModel: AddEmployerModel): Promise<EmployerModel> {
+      return new Promise(resolve => resolve({
+        id: 'any_id',
+        name: 'any_name',
+        password: 'any_password',
+        employerId: 'any_employer_id'
+      }))
+    }
+  }
+
+  return new AddEmployerRepositoryStub()
+}
+
+function makeFakeEmployer (): AddEmployerModel {
+  return {
+    name: 'any_name',
+    password: 'any_password',
+    employerId: 'any_employer_id'
+  }
+}
+
+interface SutTypes {
+  addEmployerRepositoryStub: AddEmployerRepository
+  sut: DbAddEmployer
+}
+
+function makeSut (): SutTypes {
+  const addEmployerRepositoryStub = makeAddEmployerRepositoryStub()
+  const sut = new DbAddEmployer(addEmployerRepositoryStub)
+
+  return {
+    addEmployerRepositoryStub,
+    sut
+  }
+}
+
+describe('DbAddEmployer', () => {
+  it('should call AddEmployerRepository with correct values', async () => {
+    const { sut, addEmployerRepositoryStub } = makeSut()
+    const addEmployerSpy = jest.spyOn(addEmployerRepositoryStub, 'addEmployer')
+    const fakeEmployer = makeFakeEmployer()
+    await sut.add(fakeEmployer)
+    expect(addEmployerSpy).toHaveBeenCalledWith(fakeEmployer)
+  })
+})
