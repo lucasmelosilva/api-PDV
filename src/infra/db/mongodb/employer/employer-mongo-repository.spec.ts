a99fe14d933204d3b5c844ffaf3bbf60dbc88670
Lucas Melo <lucas.melo044@gmail.com>
2025-02-11 22:00:06 -0300

    test: ensure EmployerMongoRepository returns an employer on success

---
+++ src/infra/db/mongodb/employer/employer-mongo-repository.spec.ts
@@ -0,0 +1,41 @@
+import { Collection } from 'mongodb'
+import { AddEmployerModel } from '../../../../domain/usecase/employer/add-employer'
+import { MongoHelper } from '../../../helpers/mongo-helper'
+import { EmployerMongoRepository } from './employer-mongo-repository'
+
+function makeFakeEmployer (): AddEmployerModel {
+  return {
+    name: 'any_name',
+    password: 'any_password',
+    employerId: 'any_employer_id'
+  }
+}
+let employerCollection: Collection
+
+describe('EmployerMongoRepository', () => {
+  beforeAll(async () => {
+    const url: string = process.env.MONGO_URL || ''
+    await MongoHelper.connect(url)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    employerCollection = MongoHelper.getCollection('employers')
+    await employerCollection.deleteMany({})
+  })
+
+  describe('addEmployer()', () => {
+    it('should return an employer on success', async () => {
+      const sut = new EmployerMongoRepository()
+      const result = await sut.addEmployer(makeFakeEmployer())
+      expect(result).toBeTruthy()
+      expect(result.id).toBeTruthy()
+      expect(result.name).toBe('any_name')
+      expect(result.password).toBe('any_password')
+      expect(result.employerId).toBe('any_employer_id')
+    })
+  })
+})
