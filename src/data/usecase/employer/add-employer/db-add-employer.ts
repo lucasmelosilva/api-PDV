37a7a307668dcd51869b5f5917d2562e4743a900
Lucas Melo <lucas.melo044@gmail.com>
2025-02-11 22:34:32 -0300

    feat: ensure DbAddEmployer calls Hasher with correct value

--- src/data/usecase/employer/add-employer/db-add-employer.ts
+++ src/data/usecase/employer/add-employer/db-add-employer.ts
@@ -1,14 +1,18 @@
 import { EmployerModel } from '../../../../domain/models/employer-model'
 import { AddEmployer, AddEmployerModel } from '../../../../domain/usecase/employer/add-employer'
+import { Hasher } from '../../../protocol/cryptography/hasher'
 import { AddEmployerRepository } from '../../../protocol/employer/add-employer-repository'
 
 export class DbAddEmployer implements AddEmployer {
   constructor (
+    private readonly hasher: Hasher,
     private readonly addEmployerRepository: AddEmployerRepository
   ) {}
 
   async add (addEmployerModel: AddEmployerModel): Promise<EmployerModel> {
-    const result = await this.addEmployerRepository.addEmployer(addEmployerModel)
+    const { password } = addEmployerModel
+    const hashedPassword = await this.hasher.hash(password)
+    const result = await this.addEmployerRepository.addEmployer({ ...addEmployerModel, password: hashedPassword })
     return result
   }
 }
