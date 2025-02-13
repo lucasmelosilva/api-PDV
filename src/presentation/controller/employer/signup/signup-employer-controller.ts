ec3e1abacb918ad2f17c4759b1f0a50f0ce71fb8
Lucas Melo <lucas.melo044@gmail.com>
2025-02-12 22:13:31 -0300

    feat: ensure SignUpEmployerController returns 500 if AddEmployer throws

--- src/presentation/controller/employer/signup/signup-employer-controller.ts
+++ src/presentation/controller/employer/signup/signup-employer-controller.ts
@@ -1,6 +1,7 @@
 import { AddEmployer } from '../../../../domain/usecase/employer/add-employer'
 import { badRequest } from '../../../helper/http/bad-request'
 import { ok } from '../../../helper/http/ok'
+import { serverError } from '../../../helper/http/server-error'
 import { Controller } from '../../../protocols/controller-protocol'
 import { HttpRequest } from '../../../protocols/http-request-protocol'
 import { HttpResponse } from '../../../protocols/http-response-protocol'
@@ -13,13 +14,17 @@ export class SignUpEmployerController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
-    }
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
 
-    const { passwordToConfirm, ...toAdd } = httpRequest.body
-    const result = await this.addEmployer.add(toAdd)
-    return ok(result)
+      const { passwordToConfirm, ...toAdd } = httpRequest.body
+      const result = await this.addEmployer.add(toAdd)
+      return ok(result)
+    } catch (e) {
+      return serverError(e)
+    }
   }
 }
