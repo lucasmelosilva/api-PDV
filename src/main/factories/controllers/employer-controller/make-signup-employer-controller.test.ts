19e91e1e86d98da9812faaf73cdadd7c1e72b6cc
Lucas Melo <lucas.melo044@gmail.com>
2025-02-26 20:52:51 -0300

    test: move SignUpEmployerController creation to a factory method

---
+++ src/main/factories/controllers/employer-controller/make-signup-employer-controller.test.ts
@@ -0,0 +1,10 @@
+import { SignUpEmployerController } from '../../../../presentation/controller/employer/signup/signup-employer-controller'
+
+import { makeSignupEmployerController } from './make-signup-employer-controller'
+
+describe('makeSignupEmployerController', () => {
+  it('should make SignupEmployerController', () => {
+    const sut = makeSignupEmployerController()
+    expect(sut).toBeInstanceOf(SignUpEmployerController)
+  })
+})
