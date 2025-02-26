cfba460aa491d05324b7d6731f3c8c6dc598e7b1
Lucas Melo <lucas.melo044@gmail.com>
2025-02-26 19:28:34 -0300

    test: move DbAddEmployer creation to a factory method

---
+++ src/main/factories/db/employer/make-db-add-employer.spec.ts
@@ -0,0 +1,10 @@
+import { DbAddEmployer } from '@/data/usecase/employer/add-employer/db-add-employer'
+
+import { makeDbAddEmployer } from './make-db-add-employer'
+
+describe('MakeDbAddEmployer', () => {
+  it('should return an instance of AddEmployer', () => {
+    const sut = makeDbAddEmployer()
+    expect(sut).toBeInstanceOf(DbAddEmployer)
+  })
+})
