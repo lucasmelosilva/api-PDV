f3ae9a1174fa8f3bc4deb04ff74c50ca1a2f8938
Lucas Melo <lucas.melo044@gmail.com>
2025-02-28 19:57:57 -0300

    feat: move ValidationComposite for SignUpEmployerController to a factory method

---
+++ src/main/factories/validation/make-signup-validation.ts
@@ -0,0 +1,13 @@
+import { Validation } from '../../../presentation/protocols/validation-protocol'
+import { ValidationComposite } from '../../../presentation/helper/validator/validation-composite'
+import { RequiredFieldValidation } from '@/presentation/helper/validator/required-field-validation'
+import { PasswordConfirmationValidation } from '@/presentation/helper/validator/password-confirmation-validation'
+
+export function makeSignUpValidation (fields: string[]): ValidationComposite {
+  const validations: Validation[] = []
+  for (const field of fields) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  validations.push(new PasswordConfirmationValidation())
+  return new ValidationComposite(validations)
+}
