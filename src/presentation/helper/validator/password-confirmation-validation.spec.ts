abd26262d184c2281cde61f9094faa43b624141c
Lucas Melo <lucas.melo044@gmail.com>
2025-02-28 19:41:50 -0300

    test: ensure PasswordConfirmationValidation returns InvalidParamError if validation fails

---
+++ src/presentation/helper/validator/password-confirmation-validation.spec.ts
@@ -0,0 +1,11 @@
+import { InvalidParamError } from '../../errors/invalid-param-error'
+
+import { PasswordConfirmationValidation } from './password-confirmation-validation'
+
+describe('PasswordConfirmation Validation', () => {
+  it('should return an InvalidParamError if validation fails', () => {
+    const sut = new PasswordConfirmationValidation()
+    const error = sut.validate({ password: 'any_password', passwordConfirmation: 'another_password' })
+    expect(error).toEqual(new InvalidParamError('password'))
+  })
+})
