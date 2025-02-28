b1ac6c4ab42d6c305a4593406408958eeb7bf49a
Lucas Melo <lucas.melo044@gmail.com>
2025-02-28 20:13:18 -0300

    fix: imports

--- src/main/factories/validation/make-signup-validation.ts
+++ src/main/factories/validation/make-signup-validation.ts
@@ -1,7 +1,7 @@
 import { Validation } from '../../../presentation/protocols/validation-protocol'
 import { ValidationComposite } from '../../../presentation/helper/validator/validation-composite'
-import { RequiredFieldValidation } from '@/presentation/helper/validator/required-field-validation'
-import { PasswordConfirmationValidation } from '@/presentation/helper/validator/password-confirmation-validation'
+import { RequiredFieldValidation } from '../../../presentation/helper/validator/required-field-validation'
+import { PasswordConfirmationValidation } from '../../../presentation/helper/validator/password-confirmation-validation'
 
 export function makeSignUpValidation (fields: string[]): ValidationComposite {
   const validations: Validation[] = []
