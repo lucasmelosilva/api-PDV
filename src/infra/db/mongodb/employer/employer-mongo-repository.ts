7177d6017dbe09c1d64bdc8ebf12b82ce08fdcbb
Lucas Melo <lucas.melo044@gmail.com>
2025-03-11 18:32:04 -0300

    feat: ensure EmployerMongoRepository returns an employer loaded by employerId

--- src/infra/db/mongodb/employer/employer-mongo-repository.ts
+++ src/infra/db/mongodb/employer/employer-mongo-repository.ts
@@ -1,11 +1,18 @@
+import { LoadEmployerByEmployerIdRepository } from 'data/protocol/employer/load-employer-by-employer-id-repository'
 import { AddEmployerRepository } from '../../../../data/protocol/employer/add-employer-repository'
 import { EmployerModel } from '../../../../domain/models/employer-model'
 import { AddEmployerModel } from '../../../../domain/usecase/employer/add-employer'
 import { MongoHelper } from '../../../helpers/mongo-helper'
 
-export class EmployerMongoRepository implements AddEmployerRepository {
+export class EmployerMongoRepository implements AddEmployerRepository, LoadEmployerByEmployerIdRepository {
   async addEmployer (addEmployerModel: AddEmployerModel): Promise<EmployerModel> {
     const result = await MongoHelper.insertAndFind(addEmployerModel, 'employers')
+    return MongoHelper.map(result)
+  }
+
+  async loadByEmployerId (employerId: string): Promise<EmployerModel> {
+    const collectionEmployers = MongoHelper.getCollection('employers')
+    const result = await collectionEmployers.findOne({ employerId })
     return MongoHelper.map(result)
   }
 }
