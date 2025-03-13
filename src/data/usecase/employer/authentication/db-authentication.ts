f95cd68e54d1db922dbad5622feadb42ffa57393
Lucas Melo <lucas.melo044@gmail.com>
2025-03-13 20:02:44 -0300

    feat: ensure DbAuthentication calls HashComparer if LoadEmployerByEmployerIdRepository succeeds

--- src/data/usecase/employer/authentication/db-authentication.ts
+++ src/data/usecase/employer/authentication/db-authentication.ts
@@ -1,13 +1,18 @@
+import { HashComparer } from 'data/protocol/cryptography/hash-comparer'
 import { LoadEmployerByEmployerIdRepository } from 'data/protocol/employer/load-employer-by-employer-id-repository'
 import { Authentication, AuthenticationParams, AuthenticationResult } from 'domain/usecase/employer/authentication'
 
 export class DbAuthentication implements Authentication {
   constructor (
-    private readonly loadEmployerByEmployerIdRepository: LoadEmployerByEmployerIdRepository
+    private readonly loadEmployerByEmployerIdRepository: LoadEmployerByEmployerIdRepository,
+    private readonly hashComparer: HashComparer
   ) {}
 
   async auth (authenticationParams: AuthenticationParams): Promise<AuthenticationResult> {
-    await this.loadEmployerByEmployerIdRepository.loadByEmployerId(authenticationParams.employerId)
+    const employer = await this.loadEmployerByEmployerIdRepository.loadByEmployerId(authenticationParams.employerId)
+    if (employer) {
+      await this.hashComparer.compare(authenticationParams.password, employer.password)
+    }
     return null
   }
 }
