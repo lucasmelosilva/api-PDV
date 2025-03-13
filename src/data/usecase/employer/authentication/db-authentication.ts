fae7568b2debaf64306e61f268b7572647a34488
Lucas Melo <lucas.melo044@gmail.com>
2025-03-13 20:46:29 -0300

    feat: ensure DbAuthentication calls Encrypter if HashComparer succeeds

--- src/data/usecase/employer/authentication/db-authentication.ts
+++ src/data/usecase/employer/authentication/db-authentication.ts
@@ -1,3 +1,4 @@
+import { Encrypter } from 'data/protocol/cryptography/encrypter'
 import { HashComparer } from 'data/protocol/cryptography/hash-comparer'
 import { LoadEmployerByEmployerIdRepository } from 'data/protocol/employer/load-employer-by-employer-id-repository'
 import { Authentication, AuthenticationParams, AuthenticationResult } from 'domain/usecase/employer/authentication'
@@ -5,13 +6,17 @@ import { Authentication, AuthenticationParams, AuthenticationResult } from 'doma
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadEmployerByEmployerIdRepository: LoadEmployerByEmployerIdRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly encrypter: Encrypter
   ) {}
 
   async auth (authenticationParams: AuthenticationParams): Promise<AuthenticationResult> {
     const employer = await this.loadEmployerByEmployerIdRepository.loadByEmployerId(authenticationParams.employerId)
     if (employer) {
-      await this.hashComparer.compare(authenticationParams.password, employer.password)
+      const isValid = await this.hashComparer.compare(authenticationParams.password, employer.password)
+      if (isValid) {
+        await this.encrypter.encrypt(employer.id)
+      }
     }
     return null
   }
