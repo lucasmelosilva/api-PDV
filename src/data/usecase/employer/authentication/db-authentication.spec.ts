9b5d3870fb0e4874b6c1be981dd78119c4ddbf41
Lucas Melo <lucas.melo044@gmail.com>
2025-03-13 19:15:38 -0300

    test: ensure DbAuthentication calls LoadEmployerByEmployerIdRepository with correct values

---
+++ src/data/usecase/employer/authentication/db-authentication.spec.ts
@@ -0,0 +1,53 @@
+import { EmployerModel } from '../../../../domain/models/employer-model'
+import { AuthenticationParams } from '../../../../domain/usecase/employer/authentication'
+import { LoadEmployerByEmployerIdRepository } from '../../../protocol/employer/load-employer-by-employer-id-repository'
+
+import { DbAuthentication } from './db-authentication'
+
+function makeLoadEmployerByEmployerIdRepository (): LoadEmployerByEmployerIdRepository {
+  class LoadEmployerByEmployerIdRepositoryStub implements LoadEmployerByEmployerIdRepository {
+    async loadByEmployerId (employerId: string): Promise<EmployerModel> {
+      return new Promise(resolve => resolve({
+        companyId: 'any_company_id',
+        employerId: 'dasdweqjojo1qew',
+        id: 'any_id',
+        name: 'any_name',
+        password: 'any_password'
+      }))
+    }
+  }
+
+  return new LoadEmployerByEmployerIdRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbAuthentication
+  loadEmployerByEmployerIdRepositoryStub: LoadEmployerByEmployerIdRepository
+}
+
+function makeSut (): SutTypes {
+  const loadEmployerByEmployerIdRepositoryStub = makeLoadEmployerByEmployerIdRepository()
+  const sut = new DbAuthentication(loadEmployerByEmployerIdRepositoryStub)
+
+  return {
+    sut,
+    loadEmployerByEmployerIdRepositoryStub
+  }
+}
+
+function makeFakeAuth (): AuthenticationParams {
+  return {
+    employerId: 'any_employer_id',
+    password: 'any_password'
+  }
+}
+
+describe('DbAuthentication', () => {
+  it('should call LoadEmployerByEmployerIdRepository with correct values', async () => {
+    const { sut, loadEmployerByEmployerIdRepositoryStub } = makeSut()
+    const loadSpy = jest.spyOn(loadEmployerByEmployerIdRepositoryStub, 'loadByEmployerId')
+    const authParams = makeFakeAuth()
+    await sut.auth(authParams)
+    expect(loadSpy).toHaveBeenCalledWith(authParams.employerId)
+  })
+})
