ab019afbeed502568b82679fe09a2f1556b53255
Lucas Melo <lucas.melo044@gmail.com>
2025-03-13 21:12:09 -0300

    feat: ensure DbAuthentication calls UpdateEmployerAccessTokenRepository if Encrypter succeeds

--- src/data/usecase/employer/authentication/db-authentication.ts
+++ src/data/usecase/employer/authentication/db-authentication.ts
@@ -1,13 +1,15 @@
 import { Encrypter } from 'data/protocol/cryptography/encrypter'
 import { HashComparer } from 'data/protocol/cryptography/hash-comparer'
 import { LoadEmployerByEmployerIdRepository } from 'data/protocol/employer/load-employer-by-employer-id-repository'
+import { UpdateEmployerAccessTokenRepository } from 'data/protocol/employer/update-employer-access-token-repository'
 import { Authentication, AuthenticationParams, AuthenticationResult } from 'domain/usecase/employer/authentication'
 
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadEmployerByEmployerIdRepository: LoadEmployerByEmployerIdRepository,
     private readonly hashComparer: HashComparer,
-    private readonly encrypter: Encrypter
+    private readonly encrypter: Encrypter,
+    private readonly updateEmployerAccessTokenRepository: UpdateEmployerAccessTokenRepository
   ) {}
 
   async auth (authenticationParams: AuthenticationParams): Promise<AuthenticationResult> {
@@ -15,7 +17,8 @@ export class DbAuthentication implements Authentication {
     if (employer) {
       const isValid = await this.hashComparer.compare(authenticationParams.password, employer.password)
       if (isValid) {
-        await this.encrypter.encrypt(employer.id)
+        const accessToken = await this.encrypter.encrypt(employer.id)
+        await this.updateEmployerAccessTokenRepository.updateAccessToken(employer.id, accessToken)
       }
     }
     return null
