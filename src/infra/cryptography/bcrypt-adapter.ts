ce1cf66c54fdb2ec8ba78a689098fc3d11f3c42a
Lucas Melo <lucas.melo044@gmail.com>
2025-03-21 20:56:38 -0300

    feat: bcryptAdapter implements HashComparer

--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -1,11 +1,16 @@
+import { HashComparer } from 'data/protocol/cryptography/hash-comparer'
 import { Hasher } from '../../data/protocol/cryptography/hasher'
 
 import bcrypt from 'bcrypt'
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashComparer {
   constructor (private readonly salt: number | string) {}
 
   async hash (plaintext: string): Promise<string> {
     return bcrypt.hash(plaintext, this.salt)
+  }
+
+  async compare (plaitext: string, digest: string): Promise<boolean> {
+    return bcrypt.compare(plaitext, digest)
   }
 }
