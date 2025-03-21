00181de908efbc93a47cb1a37f61431b4b324dd7
Lucas Melo <lucas.melo044@gmail.com>
2025-03-20 22:04:00 -0300

    feat: ensure LoginEmployerController returns 500 if Authentication throws

--- src/presentation/controller/employer/login/login-employer-controller.ts
+++ src/presentation/controller/employer/login/login-employer-controller.ts
@@ -1,5 +1,6 @@
 import { badRequest } from '../../../helper/http/bad-request'
 import { unauthorized } from '../../../helper/http/unauthorized'
+import { serverError } from '../../../helper/http/server-error'
 import { ok } from '../../../helper/http/ok'
 import { Controller } from '../../../protocols/controller-protocol'
 import { HttpRequest } from '../../../protocols/http-request-protocol'
@@ -14,15 +15,19 @@ export class LoginEmployerController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { body } = httpRequest
-    const error = this.validation.validate(body)
-    if (error) {
-      return badRequest(error)
+    try {
+      const { body } = httpRequest
+      const error = this.validation.validate(body)
+      if (error) {
+        return badRequest(error)
+      }
+      const authenticationModel = await this.authentication.auth(body)
+      if (!authenticationModel) {
+        return unauthorized()
+      }
+      return ok(authenticationModel)
+    } catch (error) {
+      return serverError(error)
     }
-    const authenticationModel = await this.authentication.auth(body)
-    if (!authenticationModel) {
-      return unauthorized()
-    }
-    return ok(authenticationModel)
   }
 }
