fd817a7dc3a9040ea894d72bd158e72052f39a5a
Lucas Melo <lucas.melo044@gmail.com>
2025-04-05 20:14:12 -0300

    test: ensure DbAddCompany calls Encrypter with correct values

---
+++ src/data/usecase/company/add-company/db-add-company.spec.ts
@@ -0,0 +1,43 @@
+import { DbAddCompany } from './db-add-company'
+import { AddCompanyModel } from '../../../../domain/usecase/company/add-company'
+import { Encrypter } from '../../../protocol/cryptography/encrypter'
+
+function makeEncrypter (): Encrypter {
+  class EncrypterStub implements Encrypter {
+    async encrypt (plaintext: string): Promise<string> {
+      return new Promise(resolve => resolve('encrypted_value'))
+    }
+  }
+  return new EncrypterStub()
+}
+
+interface SutTypes {
+  sut: DbAddCompany
+  encrypterStub: Encrypter
+}
+
+function makeSut (): SutTypes {
+  const encrypterStub = makeEncrypter()
+  const sut = new DbAddCompany(encrypterStub)
+  return {
+    sut,
+    encrypterStub
+  }
+}
+
+function makeFakeCompany (): AddCompanyModel {
+  return {
+    name: 'any_name',
+    cnpj: 'any_cnpj'
+  }
+}
+
+describe('DbAddCompany', () => {
+  it('should call Encrypter with correct values', async () => {
+    const { sut, encrypterStub } = makeSut()
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
+    const fakeCompany = makeFakeCompany()
+    await sut.add(fakeCompany)
+    expect(encryptSpy).toHaveBeenCalledWith(fakeCompany.cnpj)
+  })
+})
