f3a16bbcc4b53e9ca966e9ab052d445b418ede93
Lucas Melo <lucas.melo044@gmail.com>
2025-04-07 11:45:18 -0300

    feat: ensure DbAddCompany calls AddCompanyRepository with correct values

--- src/data/usecase/company/add-company/db-add-company.ts
+++ src/data/usecase/company/add-company/db-add-company.ts
@@ -1,14 +1,17 @@
+import { AddCompanyRepository } from 'data/protocol/company/add-company-repository'
 import { Encrypter } from 'data/protocol/cryptography/encrypter'
 import { CompanyModel } from 'domain/models/company-model'
 import { AddCompany, AddCompanyModel } from 'domain/usecase/company/add-company'
 
 export class DbAddCompany implements AddCompany {
   constructor (
-    private readonly encrypter: Encrypter
+    private readonly encrypter: Encrypter,
+    private readonly addCompanyRepository: AddCompanyRepository
   ) {}
 
   async add (addCompanyModel: AddCompanyModel): Promise<CompanyModel> {
-    await this.encrypter.encrypt(addCompanyModel.cnpj)
+    const encryptedValue = await this.encrypter.encrypt(addCompanyModel.cnpj)
+    await this.addCompanyRepository.addCompany({ ...addCompanyModel, cnpj: encryptedValue })
     return null
   }
 }
