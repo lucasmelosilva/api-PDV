d54d085af1bdf61ad105eea6433c58b1ef6edfb7
Lucas Melo <lucas.melo044@gmail.com>
2025-04-07 11:45:38 -0300

    test: ensure DbAddCompany calls AddCompanyRepository with correct values

--- src/data/usecase/company/add-company/db-add-company.spec.ts
+++ src/data/usecase/company/add-company/db-add-company.spec.ts
@@ -1,6 +1,22 @@
 import { DbAddCompany } from './db-add-company'
 import { AddCompanyModel } from '../../../../domain/usecase/company/add-company'
 import { Encrypter } from '../../../protocol/cryptography/encrypter'
+import { AddCompanyRepository } from '../../../protocol/company/add-company-repository'
+import { CompanyModel } from '../../../../domain/models/company-model'
+
+function makeAddCompanyRepository (): AddCompanyRepository {
+  class AddCompanyRepositoryStub implements AddCompanyRepository {
+    async addCompany (addCompanyModel: AddCompanyModel): Promise<CompanyModel> {
+      return new Promise(resolve => resolve({
+        id: 'any_id',
+        name: 'any_name',
+        cnpj: 'encrypted_value'
+      }))
+    }
+  }
+
+  return new AddCompanyRepositoryStub()
+}
 
 function makeEncrypter (): Encrypter {
   class EncrypterStub implements Encrypter {
@@ -14,14 +30,17 @@ function makeEncrypter (): Encrypter {
 interface SutTypes {
   sut: DbAddCompany
   encrypterStub: Encrypter
+  addCompanyRepositoryStub: AddCompanyRepository
 }
 
 function makeSut (): SutTypes {
   const encrypterStub = makeEncrypter()
-  const sut = new DbAddCompany(encrypterStub)
+  const addCompanyRepositoryStub = makeAddCompanyRepository()
+  const sut = new DbAddCompany(encrypterStub, addCompanyRepositoryStub)
   return {
     sut,
-    encrypterStub
+    encrypterStub,
+    addCompanyRepositoryStub
   }
 }
 
@@ -46,5 +65,13 @@ describe('DbAddCompany', () => {
     jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise(resolve => resolve(null as any)))
     const result = await sut.add(makeFakeCompany())
     expect(result).toBeNull()
+  })
+
+  it('should calls AddCompanyRepository with correct values', async () => {
+    const { sut, addCompanyRepositoryStub } = makeSut()
+    const addCompanySpy = jest.spyOn(addCompanyRepositoryStub, 'addCompany')
+    const fakeCompany = makeFakeCompany()
+    await sut.add(fakeCompany)
+    expect(addCompanySpy).toHaveBeenCalledWith({ ...fakeCompany, cnpj: 'encrypted_value' })
   })
 })
