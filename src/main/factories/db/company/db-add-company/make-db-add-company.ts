d02bb465a8ab0a954fcb13cca4f3421f3bbcfb32
Lucas Melo <lucas.melo044@gmail.com>
2025-04-13 20:21:34 -0300

    feat: config dotenv

--- src/main/factories/db/company/db-add-company/make-db-add-company.ts
+++ src/main/factories/db/company/db-add-company/make-db-add-company.ts
@@ -4,6 +4,7 @@ import { CryptoAdapter } from '../../../../../infra/cryptography/crypto-adapter'
 import { makeCompanyMongoRepository } from '../../../../../main/factories/infra/company-mongo-repository/make-company-mongo-repository'
 
 export const makeDbAddCompany = (): AddCompany => {
-  const cryptoAdapter = new CryptoAdapter(process.env.CRYPTOGRAPH_KEY)
+  const cryptoKey = process.env.CRYPTOGRAPH_KEY
+  const cryptoAdapter = new CryptoAdapter(cryptoKey)
   return new DbAddCompany(cryptoAdapter, makeCompanyMongoRepository())
 }
